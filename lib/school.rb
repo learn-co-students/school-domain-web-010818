4535dbadefc860fcebef1fea7a40932a51368176
krdiamond <github email address>
2018-01-11 14:38:36 -0500

    solved lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_accessor :name, :students_grade
+
+  def initialize(name="")
+    @name = name
+    @roster = {}
+  end
+
+
+  def roster
+    @roster
+  end
+
+  def add_student(name="", students_grade="") #add student's name and grade to roster
+
+    if @roster[students_grade] == nil
+      @roster[students_grade] = [name]
+    else
+      @roster[students_grade] << name
+    end
+  end
+
+  def grade(students_grade)
+    @roster[students_grade]
+  end
+
+
+
+  def sort
+    @roster.each do |students_grade, names|
+        @roster[students_grade] = names.sort
+      end
+      @roster
+  end
+end
