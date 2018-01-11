335e4b1a7306d4f186e627d5064cc11cd3761b3a
Raskovan <alexey.katalkin@gmail.com>
2018-01-11 13:21:52 -0500

    school

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+require 'pry'
+
+class School
+  attr_accessor :name, :roster
+
+
+def initialize(name)
+  @name = name
+  # @grade = grade
+  @roster = {}
+
+end
+
+def add_student(name, grade)
+  @roster[grade] ||= []
+  @roster[grade] << name
+end
+
+def grade(grade)
+  @roster[grade]
+end
+
+def sort
+  @roster.each_value do |names|
+    names.sort!
+  end
+end
+
+end
+
+# start binding.pry
