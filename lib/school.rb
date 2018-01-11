b0ecdad68b40e2901953dbf6c358388c32ddb463
Vaibhav Patel <vp932712@gmail.com>
2018-01-11 09:17:18 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+
+class School
+attr_accessor :highSchoolName, :roster
+
+def initialize(highSchoolName)
+  @highSchoolName = highSchoolName
+  @roster = {}
+end
+
+def add_student(name, grade)
+
+    if roster.key?(grade)
+      roster[grade].push(name)
+    else
+      roster[grade] = [].push(name)
+    end
+
+end
+
+def grade(grade)
+  roster[grade]
+
+end
+
+def sort()
+  final = Hash.new
+  a = roster.keys.sort
+  a.each do |word|
+    final[word] = roster[word].sort!
+  end
+  final
+
+
+
+end
+
+end
