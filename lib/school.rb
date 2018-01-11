422b7b5eeeea6eb41c90fbdee24efe1e4dab06f6
MinesJA <minesja@gmail.com>
2018-01-10 21:28:36 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
-# code here!
+require 'pry'
+
+class School
+
+
+
+  def initialize(name)
+    @name = name
+
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if roster.keys.include?(grade)
+      roster[grade] << name
+    else
+      roster[grade] = []
+      roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    roster.select do |elem|
+      elem == grade
+    end[grade]
+  end
+
+  def sort
+    roster.each do |grade, names|
+      sorted_names = names.sort
+      roster[grade] = sorted_names
+    end.sort.to_h
+
+  end
+
+end
